95199b3f32805fb1db2b099ec14ff85b9545bbb9
GajelliBhanusri <bhanusrig2002@gmail.com>
2025-09-18 21:43:56 +0530

    Implement stopwatch with start, stop and reset buttons

---
+++ script.js
@@ -0,0 +1,48 @@
+let sec=0;
+let min=0;
+let hour=0;
+let time="";
+let interval=null;
+let startbtn=document.getElementById("startbtn");
+let stopbtn=document.getElementById("stopbtn");
+let resetbtn=document.getElementById("resetbtn");
+function start(){
+    if(!interval){
+        interval=setInterval(() => {
+            sec++;
+            time=`${hour}:${min}:${sec}`;
+            startbtn.disabled=true;
+            if(sec>=60)
+            {
+                min++;
+                sec=0;
+            }
+            if(min>=60){
+                hour++;
+                min=0;
+                sec=0;
+            }
+            let timerElem=document.getElementById("timer");
+            timerElem.innerHTML=time;
+        }, 1000);
+    }
+}
+
+function stop(){
+    clearInterval(interval);
+    interval=null;
+    startbtn.disabled=false;
+
+}
+function reset(){
+    clearInterval(interval);
+    interval=null;
+    sec=0;
+    min=0;
+    hour=0;
+    time =`${hour}${hour}:${min}${min}:${sec}${sec}`;
+    let timerElem = document.getElementById("timer");
+    timerElem.innerHTML = time;
+    startbtn.disabled=false;
+}
+
